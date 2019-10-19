2d1ca9924a9e9851bc329efc70c7f533d8da2d09
johnsnored <56579869+johnsnored@users.noreply.github.com>
2019-10-20 03:37:24 +0530

    Pattern.py
    pattern

--- script/README.py
+++ script/README.py
@@ -1,2 +1,8 @@
-# Add your favorite script in any language
-print("Hello World")
+def pypart(n): 
+    for i in range(0, n): 
+        for j in range(0, i+1): 
+            print("* ",end="") 
+        print("\r") 
+n = 5
+pypart(n) 
+#pattern is my favourite because all kinds of complex patterns can be printed by a simple code.
